9c0a922e535d1bb83bb2ddbbd285c5d3b323827a
Mithila Khan <mithilakhan082@gmail.com>
2025-01-25 14:06:12 +0600

    sdf

--- src/redux/base/baseApi.ts
+++ src/redux/base/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://10.0.80.75:5000/api/v1" ,
+    baseUrl: "http://147.93.85.29:5000/api/v1" ,
       // baseUrl: "http://192.168.10.15:8000/api/v1" , 
       prepareHeaders: (headers) => {
         const token = GetLocalStorage("tradeToken");
@@ -19,5 +19,5 @@ export const baseApi = createApi({
 });
 
 // export const imageUrl = "http://192.168.10.15:8000/";
-export const imageUrl = "http://10.0.80.75:5000/"; 
-export const socketURL = "http://10.0.80.75:5000" 
+export const imageUrl = "http://147.93.85.29:5000/"; 
+export const socketURL = "http://147.93.85.29:5000" 
